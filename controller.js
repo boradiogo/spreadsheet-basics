12656ffcba838837a5cf393ab2f51a9984ddc2ba
Diogo Martins <sim.boradiogo@gmail.com>
2024-02-02 15:55:58 -0300

    Add function to check if student is approved

--- controller.js
+++ controller.js
@@ -5,7 +5,26 @@ const range = 'engenharia_de_software!A4:H'
 
 function calcAverage(grades) {
   return Math.round(
-    grades.reduce((sum, grade) => sum + Number(grade), 0) / grades.length)
+    grades.reduce((sum, grade) => sum + Number(grade), 0) / grades.length / 10)
+}
+
+function checkAttendance(attendance) {
+  return (attendance / 60) * 100 > 25 ? true: false
+}
+
+function checkApproval(average, attendance) {
+  const hasLackOfAttendance = checkAttendance(attendance)
+  const isApproved = average >= 7 ? true : false 
+  const isNotApprovedYet = !isApproved
+  const secondChange = average >= 5 && isNotApprovedYet ? true : false
+  const gradeToBeApproved = secondChange ? 7 * 2 - average : 0
+
+  return {
+    hasLackOfAttendance,
+    isApproved: false,
+    secondChange,
+    gradeToBeApproved
+  }
 }
 
 export async function getStudents() {
@@ -14,6 +33,7 @@ export async function getStudents() {
   const students = rows.map((row) => {
     const [id, name, attendance, ...grades] = row
     const average = calcAverage(grades)
+    const status = checkApproval(average, attendance)
     
     return {
       id,
@@ -21,6 +41,7 @@ export async function getStudents() {
       attendance,
       grades,
       average,
+      status
     }
 
   })
