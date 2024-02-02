95acc73ec10796ab23f09a2d69cb9b56d5da460a
Diogo Martins <sim.boradiogo@gmail.com>
2024-02-02 15:35:41 -0300

    Add function to calculate students average grade

--- controller.js
+++ controller.js
@@ -3,17 +3,24 @@ import { readSpreadsheet } from './model.js'
 const spreadsheetId = '1wV3XvZsmk67amFtLTqUu_Qxx8DT0MfW6_PGS6s6l_a4'
 const range = 'engenharia_de_software!A4:H'
 
+function calcAverage(grades) {
+  return Math.round(
+    grades.reduce((sum, grade) => sum + Number(grade), 0) / grades.length)
+}
+
 export async function getStudents() {
   const rows = await readSpreadsheet(spreadsheetId, range)  
 
   const students = rows.map((row) => {
     const [id, name, attendance, ...grades] = row
+    const average = calcAverage(grades)
     
     return {
       id,
       name,
       attendance,
       grades,
+      average,
     }
 
   })
