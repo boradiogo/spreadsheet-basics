c889691939cfc1e4ec5c241c79694da6ce1f6ca3
Diogo Martins <sim.boradiogo@gmail.com>
2024-02-03 23:00:09 -0300

    Create function tgenerate data to update

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
-import { readSpreadsheet } from './model.js'
+import { readSpreadsheet, updateSpreadsheet } from './model.js'
 
 const spreadsheetId = '1wV3XvZsmk67amFtLTqUu_Qxx8DT0MfW6_PGS6s6l_a4'
-const range = 'engenharia_de_software!A4:H'
+const range = 'engenharia_de_software!A4:F'
 
 function calcAverage(grades) {
   return Math.round(
@@ -51,5 +51,32 @@ export async function getStudents() {
 
   return students
 }
+function generateStatusMessage(student) {
+  let studentStatusToAppend;
 
-getStudents().then(console.log)
+  if (student.status.hasLackOfAttendance) {
+    studentStatusToAppend = ['Reprovado por falta', student.status.gradeToBeApproved]
+  } else if (!student.status.secondChance && !student.status.isApproved) {
+    studentStatusToAppend = ['Reprovado por nota', student.status.gradeToBeApproved]
+  } else if (student.status.secondChance && !student.status.isApproved) {
+    studentStatusToAppend = ['Exame final', student.status.gradeToBeApproved]
+  } else {
+    studentStatusToAppend = ['Aprovado', student.status.gradeToBeApproved]
+  }
+
+  return studentStatusToAppend
+}
+
+async function updateStudent() {
+
+  const data = await getStudents()
+  const studentsStatusMessage = data.map((student) => {
+    const approvalMessage = generateStatusMessage(student)
+    return approvalMessage
+  })
+  const students = data.map((student) => [student.status.isApproved, student.status.gradeToBeApproved])
+
+  const updated = await updateSpreadsheet(spreadsheetId, 'engenharia_de_software!G4:H', 'USER_ENTERED', studentsStatusMessage)
+}
+
+updateStudent()
